66a2bf72ee604adeb02186068e013cb25989e9d1
İzel Çavuşoğlu <izel.cavusoglu@hotmail.com>
2013-11-14 16:56:57 +0200

    2A-2b eklendi.işlem tamamlanma süresindeki fark ve yüzde hesabı hariç

--- Project-2A/Project2.2/Program.cs
+++ Project-2A/Project2.2/Program.cs
@@ -13,18 +13,46 @@ namespace Project2._2
             Console.WriteLine("kac tane araba olusturmak istersiniz");
             int n = Int16.Parse(Console.ReadLine());
             Queue<Araba> kuyruk = new Queue<Araba>();
-
+            int[] bs=new int[n];
+            OncelikKuyrugu on = new OncelikKuyrugu();
             for (int i = 0; i < n; i++)
             {
                 int a = rnd.Next(235) + 15;
-                kuyruk.Enqueue(new Araba(i, a));
-                int[] bs = beklemeSuresiBul(kuyruk);
+                Araba araba = new Araba(i, a);
+                kuyruk.Enqueue(araba);
+                on.ekle(araba);
+               
+                //kuyruk.Enqueue(new Araba(i, a));
+                bs = beklemeSuresiBul(kuyruk);
                 Console.WriteLine(String.Format("{0,-8}{1,-10}{2,-20}", "Sira No", "İs.Sur", "Bekleme"));
-                Console.WriteLine(String.Format("{0,-8}{1,-10}{2,-20}", (kuyruk.ElementAt(i).sira + 1), kuyruk.ElementAt(i).iss, bs[i]));
+                Console.WriteLine(String.Format("{0,-8}{1,-10}{2,-20}", (kuyruk.ElementAt(i).sira ), kuyruk.ElementAt(i).islemSuresi, bs[i]));
             }
-
             Console.WriteLine("ortalama bekleme suresi: " + ortalamaBul(beklemeSuresiBul(kuyruk)));
 
+            Queue<Araba> sl = new Queue<Araba>();   //sl=sıralanmış liste
+            for (int i = 0; i < kuyruk.Count; i++)
+            {
+                sl.Enqueue(on.cikar());
+            }
+
+            int[] slbs = beklemeSuresiBul(sl);  //slbs=sıralı listenin bekleme süresi listesi
+            Console.WriteLine("\n\n\nISLEM SURESI EN KISA OLAN ARABA ONCELİKLİ CIKACAK SEKİLDE KUYRUK OLUSTURULDU.\n\n");
+            for (int i = 0; i < sl.Count; i++)
+            {
+                Console.WriteLine(String.Format("{0,-8}{1,-10}{2,-20}", "Sira No", "İs.Sur", "Bekleme"));
+                Console.WriteLine(String.Format("{0,-8}{1,-10}{2,-20}", (sl.ElementAt(i).sira ), sl.ElementAt(i).islemSuresi, slbs[i]));
+            }
+
+            Console.WriteLine("ortalama bekleme suresi: " + ortalamaBul(beklemeSuresiBul(sl)));
+
+            for (int i = 0; i < n; i++)
+            {
+                if (sl.ElementAt(i).sira!= i|| slbs[i]!=bs[i]) {
+                    Console.WriteLine(kuyruk.ElementAt(i).ToString());
+                }
+            }
+
+           
 
             Console.Read();
         }
@@ -36,13 +64,15 @@ namespace Project2._2
             {
                 for (int j = i; j >= 0; j--)
                 {
-                    bs[i] += kuyruk.ElementAt(j).iss;
+                    bs[i] += kuyruk.ElementAt(j).islemSuresi;
                 }
 
             }
 
             return bs;
         }
+
+
 
         public static double ortalamaBul(int[] dizi)
         {
