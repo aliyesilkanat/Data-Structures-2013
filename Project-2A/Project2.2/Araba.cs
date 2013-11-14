987f480b787392b4769a93a4bba535b4ad99756d
izelcavusoglu <izel.cavusoglu@hotmail.com>
2013-11-14 13:25:42 +0200

    proje 2a.2 a

---
+++ Project-2A/Project2.2/Araba.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project2._2
+{
+    class Araba
+    {
+        public int iss; // iss=islem suresi
+        public int sira;
+
+
+
+        public Araba(int sira, int iss)
+        {
+            this.sira = sira;
+            this.iss = iss;
+        }
+
+  
+    }
+}
--- Project-2A/Project2.2/Program.cs
+++ Project-2A/Project2.2/Program.cs
@@ -6,23 +6,55 @@ using System.Text;
 namespace Project2._2
 {
     class Program
-    {   
+    {
         static void Main(string[] args)
         {
-            NewMethod();
+            Random rnd = new Random();
+            Console.WriteLine("kac tane araba olusturmak istersiniz");
+            int n = Int16.Parse(Console.ReadLine());
+            Queue<Araba> kuyruk = new Queue<Araba>();
 
-            Console.ReadLine();
-        }
-
-        private static void NewMethod()
-        {
-            Stack<string> a;
-            Stack<int> s = new Stack<int>();
-            s.Push(29);
-            foreach (var item in s)
+            for (int i = 0; i < n; i++)
             {
-                Console.WriteLine(item);
+                int a = rnd.Next(235) + 15;
+                kuyruk.Enqueue(new Araba(i, a));
+                int[] bs = beklemeSuresiBul(kuyruk);
+                Console.WriteLine(String.Format("{0,-8}{1,-10}{2,-20}", "Sira No", "İs.Sur", "Bekleme"));
+                Console.WriteLine(String.Format("{0,-8}{1,-10}{2,-20}", (kuyruk.ElementAt(i).sira + 1), kuyruk.ElementAt(i).iss, bs[i]));
             }
+
+            Console.WriteLine("ortalama bekleme suresi: " + ortalamaBul(beklemeSuresiBul(kuyruk)));
+
+
+            Console.Read();
         }
+
+        public static int[] beklemeSuresiBul(Queue<Araba> kuyruk)
+        {
+            int[] bs = new int[kuyruk.Count];  //bs=arabaların bekleme süresinin listesi
+            for (int i = 0; i < kuyruk.Count; i++)
+            {
+                for (int j = i; j >= 0; j--)
+                {
+                    bs[i] += kuyruk.ElementAt(j).iss;
+                }
+
+            }
+
+            return bs;
+        }
+
+        public static double ortalamaBul(int[] dizi)
+        {
+            int toplam = 0;
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                toplam += dizi[i];
+            }
+
+            return (double)toplam / dizi.Length;
+        }
+
     }
+
 }
