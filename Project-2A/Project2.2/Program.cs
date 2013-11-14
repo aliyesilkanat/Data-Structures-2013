61f5b8526140938aacb18f253111baed66a48e34
izelc <izel.cavusoglu@hotmail.com>
2013-11-14 13:23:07 +0200

    Proje 2A'nın 2.bölümünün a şıkkı tamamlandı

--- Project-2A/Project2.2/Program.cs
+++ Project-2A/Project2.2/Program.cs
@@ -6,9 +6,23 @@ using System.Text;
 namespace Project2._2
 {
     class Program
-    {
+    {   
         static void Main(string[] args)
         {
+            NewMethod();
+
+            Console.ReadLine();
+        }
+
+        private static void NewMethod()
+        {
+            Stack<string> a;
+            Stack<int> s = new Stack<int>();
+            s.Push(29);
+            foreach (var item in s)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
