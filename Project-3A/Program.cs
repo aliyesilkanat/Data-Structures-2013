8d415f7f053aee0ea72359875857ef5a46fa0852
Ali Yesilkanat <aliyesilkanat@gmail.com>
2013-12-04 00:21:09 +0200

    Sirketler text dosyasından parse edildi

--- Project-3A/Program.cs
+++ Project-3A/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
 
 namespace Project_3A
 {
@@ -9,6 +10,51 @@ namespace Project_3A
     {
         static void Main(string[] args)
         {
+            Hashtable sirketler = new Hashtable();
+            SirketleriOku(sirketler);
+            Console.Read();
+        }
+
+        private static void SirketleriOku(Hashtable sirketler)
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader(Environment.CurrentDirectory + @"\sirket.txt");
+            string line;
+            Sirket s;
+            while ((line = file.ReadLine()) != null)
+            {
+                s = new Sirket();
+
+                int i = 11;
+                s.SirketAdi = line[i].ToString();
+                while (line[++i] != '\t')
+                    s.SirketAdi += line[i];
+
+                i = line.IndexOf("Tam Adresi ") + 10;
+                s.TamAdresi = line[++i].ToString();
+                while (line[++i] != '\t')
+                    s.TamAdresi += line[i];
+
+                i = line.IndexOf("Telefon ") + 7;
+                s.TelefonNo = line[++i].ToString();
+                while (line[++i] != '\t')
+                    s.TelefonNo += line[i];
+
+                i = line.IndexOf("Faks") + 4;
+                s.Faks = line[++i].ToString();
+                while (line[++i] != '\t')
+                    s.Faks += line[i];
+
+                i = line.IndexOf("E-Posta") + 7;
+                s.EPosta = line[++i].ToString();
+                while (++i != line.Length)
+                    s.EPosta += line[i];
+
+                sirketler[s.SirketAdi] = s;
+                Console.WriteLine(s.ToString());
+
+            }
+
+            file.Close();
         }
     }
 }
---
+++ Project-3A/Sirket.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_3A
+{
+    class Sirket
+    {
+        string sirketAdi;
+
+        public string SirketAdi
+        {
+            get { return sirketAdi; }
+            set { sirketAdi = value; }
+        }
+        string tamAdresi;
+
+        public string TamAdresi
+        {
+            get { return tamAdresi; }
+            set { tamAdresi = value; }
+        }
+        string telefonNo;
+
+        public string TelefonNo
+        {
+            get { return telefonNo; }
+            set { telefonNo = value; }
+        }
+        string faks;
+
+        public string Faks
+        {
+            get { return faks; }
+            set { faks = value; }
+        }
+        string ePosta;
+
+        public string EPosta
+        {
+            get { return ePosta; }
+            set { ePosta = value; }
+        }
+        public override string ToString()
+        {
+            return sirketAdi + " " + tamAdresi + " " + telefonNo + " " + faks + " " + ePosta;
+        }
+    }
+}
